4e4256f109fc4344113fe40cb14b43314b6fb860
2cents <connortstokes@gmail.com>
2015-08-05 21:05:02 -0400

    removed trailing slash from edit url

--- edit/urls.py
+++ edit/urls.py
@@ -5,5 +5,5 @@ from edit import views
 
 urlpatterns = patterns('',
     url(r'^myedits/$', views.myedits, name='myedits'),
-    url(r'^(?P<hash_id>[^/]+)/$', views.edit, name='edit'),
+    url(r'^(?P<hash_id>[^/]+)$', views.edit, name='edit'),
 )
